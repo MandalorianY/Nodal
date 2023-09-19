236a935072447fe52c31772553e112294bface51
MandalorianY <amaurylange@gmail.com>
2023-09-19 15:05:41 +0200

    added google analytic

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello from Space! 🚀"
+def hello_world():
+    prefix_google = """ 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ELHCY793ER"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'G-ELHCY793ER');
+    </script>
+    """
+    return prefix_google + "Hello World"
