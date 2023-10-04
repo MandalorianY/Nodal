c2fc1e79bab1ae18a33a878e181d027aa56d521a
MandalorianY <amaurylange@gmail.com>
2023-10-04 15:43:12 +0200

    Logger Text box

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -10,12 +9,14 @@ def root():
     return render_template("index.html")
     
 
-@app.route("/logger")
+@app.route("/logger", methods=['GET', 'POST'])
 def logger():
     logging.info("This is a log message")
-    log_message = "This is a log message"
+    log_message="This a log message"
+    if request.method == 'POST':
+        log_message = request.form.get('Request')
+        logging.info(log_message)
+        return render_template("logger.html", log_message=log_message)
     return render_template("logger.html", log_message=log_message)
-
-
 if __name__ == "__main__":
     app.run(debug=True)
